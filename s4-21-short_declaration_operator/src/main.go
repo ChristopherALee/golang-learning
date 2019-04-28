aae724abeb0984bb07b49a94692028a206d64edd
ChristopherALee <christopheradamlee@gmail.com>
2019-04-28 12:10:01 -0400

    s4-21 short declaration operator

---
+++ s4-21-short_declaration_operator/src/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	// short declarative operator
+	x := 8
+	fmt.Println(x) // 8
+	x = 88
+	fmt.Println(x) // 88
+	y := 100 + 25
+	fmt.Println(y) // 125
+}
