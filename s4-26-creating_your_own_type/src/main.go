8af57a76af710d9099f89d092df9db27c015b048
ChristopherALee <christopheradamlee@gmail.com>
2019-04-28 12:56:49 -0400

    s4-26 creating your own type

---
+++ s4-26-creating_your_own_type/src/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+var a int
+
+type hotdog int
+
+var b hotdog
+
+func main() {
+	a = 42
+	b = 43
+	fmt.Println(a)        // 42
+	fmt.Printf("%T\n", a) // int
+	fmt.Println(b)        // 43
+	fmt.Printf("%T\n", b) // main.hotdog
+
+	// CANNOT do this. Two different types.
+	// STATIC programming language
+	// 'a' is already declared of type INT
+	// 'b' is declared of type HOTDOG
+	// a = b
+}
